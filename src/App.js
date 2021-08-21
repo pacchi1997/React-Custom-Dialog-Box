71a338b2f5f0e6dbcc8b29ad2193e7c50a707d10
prashanth kumar <32808287+pacchi1997@users.noreply.github.com>
2021-08-21 20:21:31 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,29 @@
+import React, { useState } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import AddUser from "./Components/Users/AddUser";
+import UserList from "./Components/Users/UserList";
+import Wrapper from "./Components/Helpers/Wrapper";
+import Card from "./Components/UI/Card";
 
+function App() {
+  const [myUserData, setMyUserData] = useState([]);
+
+  const addUserHandler = (userData) => {
+    setMyUserData((prevState) => {
+      return [
+        { name: userData.name, age: userData.age, id: userData.id },
+        ...prevState,
+      ];
+    });
+    console.log(myUserData);
+  };
+
+  return (
+    <Wrapper>
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={myUserData} />
+    </Wrapper>
+  );
+}
+
+export default App;
